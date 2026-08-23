a254606594714ce7d936242bf1bb881c1998db55
DreamCast <819907+MacSummer@users.noreply.github.com>
2026-08-23 12:31:19 +0800

    Release 1.0.0 unpackaged installer with external markdown reload.

--- Dev/Typedown.Core/ViewModels/SettingsViewModel.cs
+++ Dev/Typedown.Core/ViewModels/SettingsViewModel.cs
@@ -39,6 +39,8 @@ namespace Typedown.Core.ViewModels
         public bool AutoPairMarkdownSyntax { get => GetSettingValue(true); set => SetSettingValue(value); }
         public string EditorAreaWidth { get => GetSettingValue("1200px"); set => SetSettingValue(value); }
         public bool AutoSave { get => GetSettingValue(false); set => SetSettingValue(value); }
+        public bool AutoReload { get => GetSettingValue(true); set => SetSettingValue(value); }
+        public bool AskBeforeReload { get => GetSettingValue(true); set => SetSettingValue(value); }
         public AppTheme AppTheme { get => GetSettingValue(AppTheme.Default); set => SetSettingValue(value); }
         public string Language { get => GetSettingValue("default"); set => SetSettingValue(value); }
         public int WordCountMethod { get => GetSettingValue(0); set => SetSettingValue(value); }
